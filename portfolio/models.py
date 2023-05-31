ae1f7d91eabeaeea4f3ce5c92d2c0a86b2b4b9ce
Miguel <a22102861@alunos.ulht.pt>
2023-05-31 09:14:43 +0100

    change to models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,4 +1,7 @@
+from collections import UserDict
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Tarefa(models.Model):
@@ -12,6 +15,19 @@ class Tarefa(models.Model):
     
 
 
+class Blog(models.Model):
+    utilizador = models.OneToOneField(User, on_delete=models.CASCADE)
+    github_link = models.URLField(max_length=200, blank=True)
+    pythonanywhere_link = models.URLField(max_length=200, blank=True)
+
+class Area(models.Model):
+    nome = models.CharField(max_length=100)
+
+class Autor(models.Model):
+    utilizador = models.OneToOneField(User, on_delete=models.CASCADE)
+    areasDeInteresse = models.ManyToManyField(Area)
+    
+
 class Post(models.Model):
     
     autor = models.CharField(max_length=100)
@@ -19,10 +35,16 @@ class Post(models.Model):
     titulo = models.CharField(max_length=25)
     descricao = models.TextField()
     link = models.CharField(max_length=100, blank=True)
-    
+    #imagem = models.ImageField(upload_to='images/', blank=True)
 
+class Comentario(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comentarios')
+    titulo = models.CharField(max_length=50)
+    texto = models.TextField()
 
-
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    utilizador = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.titulo
     
