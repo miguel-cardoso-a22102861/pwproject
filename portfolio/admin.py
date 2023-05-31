0845b0b6c3459a35b765166548760d7e621612f7
Miguel <a22102861@alunos.ulht.pt>
2023-05-31 14:44:40 +0100

    models?

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Post, Tarefa
+from .models import Area, Autor, Blog, BlogOwner, Comentario, Post, Tarefa
 # Register your models here.
 admin.site.register(Tarefa)
 
 admin.site.register(Post)
+admin.site.register(Area)
+admin.site.register(Autor)
+admin.site.register(Blog)
+admin.site.register(Comentario)
+admin.site.register(BlogOwner)
--- portfolio/models.py
+++ portfolio/models.py
@@ -12,40 +12,55 @@ class Tarefa(models.Model):
 
     def __str__(self):
         return self.titulo
-    
 
-
-class Blog(models.Model):
-    utilizador = models.OneToOneField(User, on_delete=models.CASCADE)
+class BlogOwner(models.Model):
+    nome = models.TextField(max_length= 25)
     github_link = models.URLField(max_length=200, blank=True)
     pythonanywhere_link = models.URLField(max_length=200, blank=True)
 
 class Area(models.Model):
     nome = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nome
+
 class Autor(models.Model):
-    utilizador = models.OneToOneField(User, on_delete=models.CASCADE)
-    areasDeInteresse = models.ManyToManyField(Area)
+    nome = models.CharField(max_length=25)
+    areasDeInteresse = models.ManyToManyField(Area, related_name='autores')
+
+    
+    def __str__(self):
+        return self.nome
     
 
 class Post(models.Model):
-    
-    autor = models.CharField(max_length=100)
+
+    autores = models.ManyToManyField(Autor, related_name='posts')
     data = models.DateTimeField(auto_now_add=True)
     titulo = models.CharField(max_length=25)
     descricao = models.TextField()
     link = models.CharField(max_length=100, blank=True)
-    #imagem = models.ImageField(upload_to='images/', blank=True)
+    imagem = models.ImageField(upload_to='images/', blank=True)
+    comentarios = models.ManyToManyField('Comentario', related_name='posts')
+    like = models.IntegerField(default=0)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.titulo
+
+
+class Blog(models.Model):
+    utilizador = models.OneToOneField(BlogOwner, on_delete=models.CASCADE, unique=True)
+    areas = models.ManyToManyField(Area, related_name='blogs')
 
 class Comentario(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comentarios')
     titulo = models.CharField(max_length=50)
     texto = models.TextField()
 
-class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
-    utilizador = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.titulo
+
+
+
     
 
